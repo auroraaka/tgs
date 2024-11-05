395cab66c02332a16bc781b3e3ef59621915a240
Akarsh Aurora <akarshaurora@mac.myfiosgateway.com>
2024-11-05 01:04:10 -0500

    fix

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from src.core.fit import TGSAnalyzer
 if __name__ == '__main__':
     with open('config.yaml', "r") as file: config = yaml.safe_load(file)
     analyzer = TGSAnalyzer(config)
-    analyzer.run_analysis()
+    analyzer.run_analysis(config)
 
-    with open(analyzer.paths.signal, 'r') as file: signal = json.load(file)
-    fit = pd.read_csv(analyzer.paths.fit)
-    plot_interactive(signal, fit)
+    # with open(analyzer.paths.signal, 'r') as file: signal = json.load(file)
+    # fit = pd.read_csv(analyzer.paths.fit)
+    # plot_interactive(signal, fit)
